119ba9b3958a2d155880586c5ada64873f09140f
Bob Chiverton <robert.chiverton@hotmail.com>
2018-11-05 13:28:12 -0500

    initial commit

---
+++ get-preferences/index.ts
@@ -0,0 +1,48 @@
+import now from "performance-now";
+import { MySingletonCosmosClient, ICosmosDataWithTimings } from "../shared/cosmosClient";
+
+export async function run(context: any, req: any) {
+    context.log("Entering get-preferences operation for Personalization");
+
+    // get the user id from url
+    const id: string = req.params.id;
+    context.log("id = " + id);
+
+    const client = MySingletonCosmosClient.GetClient();
+
+    try
+    {
+    const startedAt = now();
+
+    if (id !== "") {
+        context.log("before item GET");
+        const item = await client.database(MySingletonCosmosClient.database).container(MySingletonCosmosClient.collection).item(id);
+        context.log("after item GET");
+        const i = await item.read<ICosmosDataWithTimings>();
+        context.log("after i READ ");
+
+        const endedAt = now();
+        const elapsedPost = Math.round(endedAt - startedAt);
+
+        const userPersonalDataWithTimings  = i.body as ICosmosDataWithTimings;
+        userPersonalDataWithTimings.duration = elapsedPost;
+        userPersonalDataWithTimings.preferredCosmosDBLocation = MySingletonCosmosClient.location;
+        userPersonalDataWithTimings.actualReadOrWriteEndPoint = await client.getReadEndpoitn() ;
+
+        context.res.body = userPersonalDataWithTimings;
+        }
+    else {
+        context.res = {
+                status: 400,
+                body: `$"No records found for the id: {id}"`
+            };
+        }
+    }
+    catch (e)
+        // tslint:disable-next-line:one-line
+        {
+        context.log(e);
+        context.res.body = e;
+        }
+    // context.done();
+    }
---
+++ hello/index.ts
@@ -0,0 +1,21 @@
+export async function run(context: any, req: any) {
+    context.log("TypeScript HTTP trigger function 'hello' processed a request.");
+
+    // get OAuth info (used by webpart)
+    const url      = process.env.WEBAPI_APPID_URI;
+    const appid    = process.env.WEBAPI_APPID;
+
+    const data = {
+        // tslint:disable-next-line:object-literal-key-quotes
+        "url" : url,
+        // tslint:disable-next-line:object-literal-key-quotes
+        "appid" : appid
+    };
+
+    context.res =   {
+                    // status: 200, /* Defaults to 200 */
+                    body:  data
+                    };
+
+// context.done();
+}
